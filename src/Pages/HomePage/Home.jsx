21d49d94d929ba638533ae7790f08a28e164d34b
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-04-30 16:18:13 -0300

    Login page authentication

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,9 +1,28 @@
+import "./HomeStyles.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 export const Home = () => {
+    const [booksList, setBooksList] = useState([]);
+    // useEffect(() => {
+    //     axios.get("https://books.ioasys.com.br/api/v1/books")
+    //         .then(res => {
+    //             setBooksList(res.data);
+    //             console.log(res.data);
+    //         })
+    // },[])
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home-container">
+            <div className="header-container">
+                <div>
+                    <h1 className="home-header-title">ioasys <span className="home-header-subtitle">Books</span></h1>
+                </div>
+                <div className="logout-icon">
+                    <i className="bi bi-box-arrow-right"></i>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,62 +1,71 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import './LoginStyles.css';
+import axios from "axios";
+import { Tooltip, Button } from "antd";
+import { useState } from "react";
+import "./LoginStyles.css";
 
 export const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    
-    const isLogged = () => {
-        let token = Cookies.get('token');
-        return token ? true : false;
-    }
-    const doLogin = () => {
-        axios.post('https://books.ioasys.com.br/api/v1/auth/sign-in', {
-            username: username,
-            password: password
-        }).then((res) => {
-            if (res.data.success) {
-                Cookies.set('token', res.data.token);
-                window.location.href = '/';
-            } else {
-                setError(res.data.message);
-            }
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const json = await doLogin(username, password);
-        console.log(e)
-        if(json.error){
-            setError(json.error);
-            console(json)
-        }else{
-            window.location.href = '/home'
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState();
+  const [tooltipDisplay, setTooltipDisplay] = useState(false);
+  const [error, setError] = useState();
+  
+
+  const signIn = async (e) => {
+    e.preventDefault();
+    axios
+      .post("https://books.ioasys.com.br/api/v1/auth/sign-in", {
+        email,
+        password,
+      })
+      .then((res) => {
+        if (res.data.name) {
+          setUsername(res.data.name);  
+          window.location.href = "/home";
+        } else {
+          setError("Usuário ou senha incorretos");
         }
-    }
-    return (
-        <div className='login-container'>
-            <div style={{ disply: 'flex', alignItems: 'flex-start', padding: '16px' }}>
-                <h1>ioasys <span>Books</span></h1>
-                <div className='input-container'>
-                    <h5>Email</h5>
-                    <input type="email" className='login-box' value={username} required onChange={(event) => {
-                        setUsername(event.target.value);
-                    }} />
-                </div>
-                <div className='input-container'>
-                    <h5 style={{ width: '50px' }}>Senha</h5>
-                    <input type="password" className='login-box' value={password} required onChange={(event) => {
-                        setPassword(event.target.value);
-                    }} />
-                    <button className='login-button' onClick={handleSubmit}>Entrar</button>
-                </div>
-            </div>
+      });
+  };
+
+  return (
+    <div className="login-container">
+      <div
+        style={{ disply: "flex", alignItems: "flex-start", padding: "16px" }}
+      >
+        <div className="header-login">  
+            <h1 className="main-login-title">
+            ioasys <span className='login-header-subtitle'>Books</span>
+            </h1>
         </div>
-    )
-}
+        <div className="input-container">
+          <h5>Email</h5>
+          <input
+            type="email"
+            className="login-box"
+            value={email}
+            required
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </div>
+        <div className="input-container">
+          <h5 style={{ width: "50px" }}>Senha</h5>
+          <input
+            type="password"
+            className="login-box"
+            value={password}
+            required
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <button type="button" className="login-button" onClick={signIn}>
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
